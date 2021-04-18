fc4628f8b443dc295c5c6d2407caf1bedba3bc93
gargtanmay <tanmayjdr@gmail.com>
2021-04-18 21:07:37 +0530

    Design Changes

--- main.js
+++ main.js
@@ -22,8 +22,10 @@ densebutton.addEventListener('click',(e)=>{
     inp1.classList.add('form-control')
     div.appendChild(label);
     div.appendChild(inp);
+    div.append(document.createElement("BR"));
     div.appendChild(label2);
     div.appendChild(inp1);
+    div.append(document.createElement("BR"));
     var array = ["Select","Adam","SGD"];
     const selectList = document.createElement("select");
     selectList.classList.add('form-control');
@@ -37,7 +39,11 @@ densebutton.addEventListener('click',(e)=>{
         option.text = array[i];
         selectList.appendChild(option);
     }
+    div.append(document.createElement("BR"));
     div.appendChild(savbut);
+    div.style.border="1px solid black";
+    div.style.padding="20px";
+    
     dense.appendChild(div);
    
     savbut.addEventListener('click',(e)=>{
